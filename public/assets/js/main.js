5a9bf0f0ef7af20450ebebac32906f0a916bb743
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-07-08 10:14:02 -0300

    foi criado função para enviar a mensagem quando apertar Enter e entrar no Chat

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,39 @@
 const socket = io();// se for a conexão com outro servidor colocar a url do servidor, EX: io(url do servidor)
+
+let username = '';
+let userList = [];
+
+const loginPage = document.querySelector('#login-page');
+const chatPage = document.querySelector('#chat-page');
+
+const loginInput = document.querySelector('#login-name-input');
+const textInput = document.querySelector('#text-input');
+
+loginPage.style.display = 'flex';
+chatPage.style.display = 'none';
+
+const renderUserList = () => {
+
+}
+
+loginInput.addEventListener('keyup', (e) => {
+   if (e.keyCode === 13) {
+      let name = loginInput.value.trim();
+
+      if (name !== '') {
+         username = name;
+         document.title = `Chat (${username})`;
+         socket.emit('join-request', username);
+      }
+   }
+});
+
+// vai entra no chat
+socket.on('user-ok', (list) => {
+   loginPage.style.display = 'none';
+   chatPage.style.display = 'flex';
+   textInput.focus();
+
+   userList = list;
+   renderUserList();
+});
