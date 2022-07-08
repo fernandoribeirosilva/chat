0a6792cf135cda98f077c3d74ce739c7147866a5
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-07-08 11:12:44 -0300

    foi implementado método de desconectar o usuário

--- src/connection/socketIO.js
+++ src/connection/socketIO.js
@@ -16,12 +16,23 @@ exports.io = {
             connectionUsers.push(username);
             console.log(connectionUsers);
 
-            // vai mostrar o usuário da requisição
+            // vai mostrar o usuário da requisição, e conectar o usuário 
             socket.emit('user-ok', connectionUsers);
             // vai notificar todos os usuários que um novo usuário entrou, menos o próprio usuário
             socket.broadcast.emit('list-update', {
                joined: username, // usuário que entrou
                list: connectionUsers // lista de usuários
+            });
+         });
+
+         socket.on('disconnect', () => {
+            // se achar o usuário na lista de usuários
+            connectionUsers = connectionUsers.filter(user => user !== socket.username);
+            console.log(connectionUsers);
+
+            socket.broadcast.emit('list-update', {
+               left: socket.username, // usuário que saiu
+               list: connectionUsers // nova lista de usuários
             });
          });
       });
